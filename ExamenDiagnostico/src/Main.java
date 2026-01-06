838985522a771461afb26b57c514d58f32a32f64
Jorge Moranchel <20243ds>
2026-01-06 08:42:24 -0600

    Empece el examen de diagnostico y primero cree mis dos variables y mi scanner y ahora voy a empezar el while por buenas practicas

---
+++ ExamenDiagnostico/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numeros;
+        int suma = 0;
+
+        System.out.println("La suma total es : " + suma);
+
+    }
+}
